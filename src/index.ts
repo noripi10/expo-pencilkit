89c1d84c0ec4cba3c59765341b9e7ec50b552315
noripi10 <55979374+noripi10@users.noreply.github.com>
2024-01-16 21:39:10 +0900

    remove clearDraw

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+// import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
 
 // Import the native module. On web, it will be resolved to ExpoPencilkit.web.ts
 // and on native platforms to ExpoPencilkit.ts
 import ExpoPencilkit from './ExpoPencilkit';
 import { ExpoPencilkitViewProps } from './ExpoPencilkit.types';
-import ExpoPencilkitModule from './ExpoPencilkitModule';
-
-const clearDrawAsync = async () => {
-  return await ExpoPencilkitModule.clearDrawAsync();
-};
+// import ExpoPencilkitModule from './ExpoPencilkitModule';
 
 export default ExpoPencilkit;
-export { ExpoPencilkitViewProps, clearDrawAsync };
+export { ExpoPencilkitViewProps };
